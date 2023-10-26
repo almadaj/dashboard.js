703be6784b9cdf7ef764ab252583cb5c7dc6a924
Juliano Almada <almadaj@edu.unifor.br>
2023-10-26 11:04:58 -0300

    estrutura principal e rotas finalizadas

---
+++ react-stock-app/src/App.jsx
@@ -0,0 +1,9 @@
+import router from "./router";
+import { RouterProvider } from "react-router-dom";
+export default function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
---
+++ react-stock-app/src/pages/items/CreateItem.jsx
@@ -0,0 +1,3 @@
+export default function CreateItem() {
+  return <h2>Novo Item</h2>;
+}
